0f740398df0237655f02de0ce412339b855e52ed
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-30 09:21:10 -0600

    add folder redux, books,categories and setupstore

---
+++ src/redux/setupstore.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'; 
+
+import bookReducer from './books/book';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = {
+  book: bookReducer,
+  categories: categoriesReducer,
+};
+
+const store = configureStore({ reducer: rootReducer });
+
+export default store;
