f5e1b4b704e4a3b680b61f98389d33d6d8df382a
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-30 09:03:08 -0600

    redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+    </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
