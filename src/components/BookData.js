fce2f79e1a05ae457bb677fd7bac5eb1c38f895b
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-01-30 18:05:28 -0600

    functionality react/redux

--- src/components/BookData.js
+++ src/components/BookData.js
@@ -1,52 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import BookItem from './Book';
 
-const BooksInfo = [
-  {
-    id: 0,
-    category: 'Science Fiction',
-    title: 'Perfume The Story of a Murderer',
-    author: 'Patrick Süskind',
-    chapters: 30,
-    currentChapter: 22,
-  },
-  {
-    id: 1,
-    category: 'Science Fiction',
-    title: 'The Slave: A spiritual manifesto for a better way of life',
-    author: 'Anan Dilvar',
-    chapters: 23,
-    currentChapter: 2,
-  },
-  {
-    id: 2,
-    category: 'Adventure',
-    title: 'Don Quixote of La Mancha',
-    author: 'Miguel de Cervantes',
-    chapters: 25,
-    currentChapter: 15,
-  }];
+const BookData = () => {
+  const bookList = useSelector((state) => state.book);
+  return (
+    <ul>
+      {bookList.map((book) => {
+        const {
+          id, category, title, author, chapters, currentChapter,
+        } = book;
+        return (
+          <BookItem
+            key={id}
+            category={category}
+            title={title}
+            author={author}
+            chapters={chapters}
+            currentChapter={currentChapter}
+          />
 
-const BookData = () => (
-
-  <ul>
-    {BooksInfo.map((book) => {
-      const {
-        id, category, title, author, chapters, currentChapter,
-      } = book;
-      return (
-        <BookItem
-          key={id}
-          category={category}
-          title={title}
-          author={author}
-          chapters={chapters}
-          currentChapter={currentChapter}
-        />
-
-      );
-    })}
-  </ul>
-);
+        );
+      })}
+    </ul>
+  );
+};
 
 export default BookData;
--- src/redux/books/book.js
+++ src/redux/books/book.js
@@ -1,7 +1,33 @@
 const NEW_BOOK = 'bookstore/books/NEW_BOOK';
 const DELETE_BOOK = 'bookstore/books/DELETE_BOOK';
 
-const bookReducer = (state = [], action) => {
+const startBooks = [
+  {
+    id: 0,
+    category: 'Science Fiction',
+    title: 'Perfume The Story of a Murderer',
+    author: 'Patrick Süskind',
+    chapters: 30,
+    currentChapter: 22,
+  },
+  {
+    id: 1,
+    category: 'Science Fiction',
+    title: 'The Slave: A spiritual manifesto for a better way of life',
+    author: 'Anan Dilvar',
+    chapters: 23,
+    currentChapter: 2,
+  },
+  {
+    id: 2,
+    category: 'Adventure',
+    title: 'Don Quixote of La Mancha',
+    author: 'Miguel de Cervantes',
+    chapters: 25,
+    currentChapter: 15,
+  }];
+
+const bookReducer = (state = startBooks, action) => {
   switch (action.type) {
     case NEW_BOOK:
       return [
