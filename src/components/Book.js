12e49826cf88b469376b5b0e1cf6d537416b17e6
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-02-03 09:00:21 -0600

    add all the resource to work in the body

--- src/components/Book.js
+++ src/components/Book.js
@@ -14,14 +14,49 @@ const BookItem = (props) => {
   };
 
   return (
-
-    <li className="BookFormat">
-      <h3>{category}</h3>
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <button onClick={onClick} type="button">Remove</button>
-    </li>
-
+    <>
+      <div className="container">
+        <ul className="books">
+          <li>
+            <div className="book">
+              <div className="book-content">
+                <div className="book-info">
+                  <h3 className="book-category">{category}</h3>
+                  <h2 className="book-title">{title}</h2>
+                  <h3 className="book-author">{author}</h3>
+                  <div className="action-buttons">
+                    <button className="button-outline" type="button"> comment </button>
+                    <div className="vertical-divider" />
+                    <button className="button-outline" onClick={onClick} type="button">Remove</button>
+                    <div className="vertical-divider" />
+                    <button className="button-outline" type="button"> edit </button>
+                  </div>
+                </div>
+                <div className="progress-container">
+                  <div className="circular-progress-container">
+                    <div className="circular-progress" />
+                  </div>
+                </div>
+                <div className="progress-stat">
+                  <p className="percent-complete">75%</p>
+                  <p className="completed">completed</p>
+                </div>
+                <div className="progress-divider" />
+                <div className="current-chapter-container ">
+                  <div>
+                    <p className="current-chapter-label"> CURRENT CHAPTER </p>
+                    <p className="current-chapter">chapter 14</p>
+                  </div>
+                  <div>
+                    <button className="primary-button" type="button">UPDATE PROGRESS</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
